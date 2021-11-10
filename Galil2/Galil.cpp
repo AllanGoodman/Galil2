628f891e7b40e2dd701caced0fe68d3aa9e671eb
Allan Goodman <allan.goodman@student.unsw.edu.au>
2021-11-10 15:39:02 +1100

    added digitalBitInput (not sure about functionality), added AnalogOutput

--- Galil2/Galil.cpp
+++ Galil2/Galil.cpp
@@ -21,7 +21,7 @@ Galil::~Galil() {
 
 // DIGITAL OUTPUTS
 void Galil::DigitalOutput(uint16_t value) {
-	
+	GWrite();
 }
 
 void Galil::DigitalByteOutput(bool bank, uint8_t value) {
@@ -40,28 +40,35 @@ uint16_t Galil::DigitalInput() {
 }
 
 uint8_t Galil::DigitalByteInput(bool bank) {
-
+	puts
 }
 
 bool Galil::DigitalBitInput(uint8_t bit) {
 	//Read a bit from one of the digital inputs specified by bit.
+	char command[128] = "";
+	sprintf(command, "@IN[%d]", bit);
+	Functions->GCommand(g, command, ReadBuffer, sizeof(ReadBuffer), 0);
 }
 
 bool Galil::CheckSuccessfulWrite() {
-	if (success) {
+	if (ReadBuffer == ":") {
 		return 1;
 	}
-	else {
+	else if (ReadBuffer == "?") {
 		return 0;
 	}
 }
 
-float Galil::AnalogInput(uint8_t channel) {
 
+//Analog Functions
+float Galil::AnalogInput(uint8_t channel) {
+	GRead();
 }
 
 void Galil::AnalogOutput(uint8_t channel, double voltage) {
-
+	char command[128];
+	sprintf(command, "AO %d,%f", channel, voltage);
+	GCommand(g, command, ReadBuffer, sizeof(ReadBuffer), 0);
 }
 
 void Galil::AnalogInputRange(uint8_t channel, uint8_t range) {
